85bad16b512cf82c0baf7e5727953f9da28c9b35
mehdad-hussain <hussain.mehdad1@gmail.com>
2021-12-12 18:01:08 +0600

    12/12/2021/2

--- layout/Layout.js
+++ layout/Layout.js
@@ -17,98 +17,96 @@ const Layout = ({ children, pageTitle, backOption }) => {
 
       <main className={styles.mainContent}>
         <div className={styles.main}>
-          <div className={styles.recentGrid}>
-            <div className={styles.card}>
-              <div className={styles.cardHeader}>
-                <h2>Customer List</h2>
-                <div className={styles.searchWrapper}>
-                  <input type='search' placeholder='Search' />
-                  <span className=''></span>
-                </div>
+          <div className={styles.card}>
+            <div className={styles.cardHeader}>
+              <h2>Customer List</h2>
+              <div className={styles.searchWrapper}>
+                <input type='search' placeholder='Search' />
+                <span className=''></span>
               </div>
+            </div>
 
-              <div className={styles.cardBody}>
-                <div className='table-responsive'>
-                  <table className={styles.table}>
-                    <thead>
-                      <tr>
-                        <td>Name</td>
-                        <td>Phone Number</td>
-                        <td>Gender</td>
-                        <td>Since</td>
-                        <td>Action</td>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr className={styles.line}>
-                        <td>Jemi</td>
-                        <td>017979.....</td>
-                        <td>Male</td>
-                        <td>21 june 2022</td>
-                        <td>Details</td>
-                      </tr>
-                      <tr>
-                        <td>Jemi</td>
-                        <td>017979.....</td>
-                        <td>Male</td>
-                        <td>21 june 2022</td>
-                        <td>Details</td>
-                      </tr>
-                      <tr>
-                        <td>Jemi</td>
-                        <td>017979.....</td>
-                        <td>Male</td>
-                        <td>21 june 2022</td>
-                        <td>Details</td>
-                      </tr>
-                      <tr>
-                        <td>Jemi</td>
-                        <td>017979.....</td>
-                        <td>Male</td>
-                        <td>21 june 2022</td>
-                        <td>Details</td>
-                      </tr>
-                      <tr>
-                        <td>Jemi</td>
-                        <td>017979.....</td>
-                        <td>Male</td>
-                        <td>21 june 2022</td>
-                        <td>Details</td>
-                      </tr>
-                      <tr>
-                        <td>Jemi</td>
-                        <td>017979.....</td>
-                        <td>Male</td>
-                        <td>21 june 2022</td>
-                        <td>Details</td>
-                      </tr>
-                      <tr>
-                        <td>Jemi</td>
-                        <td>017979.....</td>
-                        <td>Male</td>
-                        <td>21 june 2022</td>
-                        <td>Details</td>
-                      </tr>
-                      <tr>
-                        <td>Jemi</td>
-                        <td>017979.....</td>
-                        <td>Male</td>
-                        <td>21 june 2022</td>
-                        <td>Details</td>
-                      </tr>
-                      <tr>
-                        <td>Jemi</td>
-                        <td>017979.....</td>
-                        <td>Male</td>
-                        <td>21 june 2022</td>
-                        <td>Details</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-                <div className={styles.pagination}>
-                  <PaginationBar />
-                </div>
+            <div className={styles.cardBody}>
+              <div className={styles.tableResponsive}>
+                <table className={styles.table}>
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Phone Number</th>
+                      <th>Gender</th>
+                      <th>Since</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className={styles.line}>
+                      <td>Jemi</td>
+                      <td>017979.....</td>
+                      <td>Male</td>
+                      <td>21 june 2022</td>
+                      <td>Details</td>
+                    </tr>
+                    <tr>
+                      <td>Jemi</td>
+                      <td>017979.....</td>
+                      <td>Male</td>
+                      <td>21 june 2022</td>
+                      <td>Details</td>
+                    </tr>
+                    <tr>
+                      <td>Jemi</td>
+                      <td>017979.....</td>
+                      <td>Male</td>
+                      <td>21 june 2022</td>
+                      <td>Details</td>
+                    </tr>
+                    <tr>
+                      <td>Jemi</td>
+                      <td>017979.....</td>
+                      <td>Male</td>
+                      <td>21 june 2022</td>
+                      <td>Details</td>
+                    </tr>
+                    <tr>
+                      <td>Jemi</td>
+                      <td>017979.....</td>
+                      <td>Male</td>
+                      <td>21 june 2022</td>
+                      <td>Details</td>
+                    </tr>
+                    <tr>
+                      <td>Jemi</td>
+                      <td>017979.....</td>
+                      <td>Male</td>
+                      <td>21 june 2022</td>
+                      <td>Details</td>
+                    </tr>
+                    <tr>
+                      <td>Jemi</td>
+                      <td>017979.....</td>
+                      <td>Male</td>
+                      <td>21 june 2022</td>
+                      <td>Details</td>
+                    </tr>
+                    <tr>
+                      <td>Jemi</td>
+                      <td>017979.....</td>
+                      <td>Male</td>
+                      <td>21 june 2022</td>
+                      <td>Details</td>
+                    </tr>
+                    <tr>
+                      <td>Jemi</td>
+                      <td>017979.....</td>
+                      <td>Male</td>
+                      <td>21 june 2022</td>
+                      <td>Details</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className={styles.pagination}>
+                <PaginationBar />
               </div>
             </div>
           </div>
